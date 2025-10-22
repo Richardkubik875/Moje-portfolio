6708df05a455260de25bde44648bbe5734a7b701
Richardkubik875 <243011@student.vitkovickastredni.cz>
2025-10-22 09:03:43 +0200

    rozprc.

--- app.py
+++ app.py
@@ -1,54 +1,13 @@
 from flask import  Flask, render_template
+from data import users, nazev_webu, popis, technologie, titulek_webu
 
 app = Flask (__name__)
 
-users = {
-    1: {
-        "jmeno": "Jan",
-        "prijmeni": "Novák",
-        "email": "jan.novak@example.com",
-        "telefon": "+420123456789",
-        "kancelar": "101A"
-    },
-    2: {
-        "jmeno": "Petra",
-        "prijmeni": "Svobodová",
-        "email": "petra.svobodova@example.com",
-        "telefon": "+420987654321",
-        "kancelar": "102B"
-    },
-    3: {
-        "jmeno": "Martin",
-        "prijmeni": "Kapcala",
-        "email": "martin.kapcala@example.com",
-        "telefon": "+420456789123",
-        "kancelar": "103C"
-    },
-    4: {
-        "jmeno": "Lucie",
-        "prijmeni": "Dvořáková",
-        "email": "lucie.dvorakova@example.com",
-        "telefon": "+420321654987",
-        "kancelar": "104D"
-    },
-    5: {
-        "jmeno": "Tomáš",
-        "prijmeni": "Havlásek",
-        "email": "tomas.havlasek@example.com",
-        "telefon": "+420789123456",
-        "kancelar": "105E"
-    }
-}
 @app.route("/")
-def base():
-    nazev_webu =  "Richardovo Portfolio"
-    titulek_webu = "idk"
-    popis = "Ukázka projektů, výuka s AI, a testování Jitsi2",
-    technologie =  ["Flask", "Python", "HTML", "CSS", "Jitsi2", "Copilot"],
+def home():
+    return render_template("index.html", nazev_webu = nazev_webu, titulek_webu = titulek_webu)
 
-    return render_template("base.html", nazev_webu = nazev_webu, titulek_webu = titulek_webu)
-
-@app.route("/")
+@app.route("/contacts")
 def contacts():
     return render_template("contacts.html", users = users)
 
